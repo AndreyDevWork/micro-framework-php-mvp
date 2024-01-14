7404d40160929454534b8f2012c4bbadc1d06fab
Andrey <andrey.develop.a@gmail.com>
2024-01-14 17:39:35 +0700

    feat: Класс Db (Database)

--- app/controllers/index.php
+++ app/controllers/index.php
@@ -1,38 +1,5 @@
 <?php
-
-
-$posts = [
-  1 => [
-    'title' => 'Title1',
-    'desc' => '1Some quick example text to build on the card title and make up the bulk of the card',
-    'slug' => 'title-1',
-  ],
-  2 => [
-      'title' => 'Title2',
-      'desc' => '2Some quick example text to build on the card title and make up the bulk of the card',
-      'slug' => 'title-2',
-  ],
-  3 => [
-      'title' => 'Ti3',
-      'desc' => '3Some quick example text to build on the card title and make up the bulk of the card',
-      'slug' => 'title-3',
-  ],
-  4 => [
-      'title' => 'Title4',
-      'desc' => '4Some quick example text to build on the card title and make up the bulk of the card',
-      'slug' => 'title-4',
-  ],
-];
-
-$recentPosts = [
-  1 => [
-      'title' => 'Ti tl e4',
-      'slug' => lcfirst(str_replace(' ', '-', 'Ti tl e4')),
-  ],
-  2 => [
-      'title' => 'Ti tle 5',
-      'slug' => lcfirst(str_replace(' ', '-', 'Ti tl e5')),
-  ]
-];
+$posts = $db->query("SELECT * FROM posts ORDER BY id DESC")->fetchAll();
+$recentPosts = $db->query("SELECT * FROM posts ORDER BY id DESC LIMIT 3")->fetchAll();
 
 require VIEWS . '/index.tpl.php';
--- app/views/incs/sidebar.php
+++ app/views/incs/sidebar.php
@@ -2,7 +2,7 @@
     <h2>Recent Posts</h2>
     <ul class="list-group">
         <?php foreach ($recentPosts as $post): ?>
-            <li class="list-group-item"><a href="<?= $post['slug'] ?>"><?= $post['title'] ?></a></li>
+            <li class="list-group-item"><a href="post/?id=<?= $post['id'] ?>"><?= $post['title'] ?></a></li>
         <?php endforeach; ?>
     </ul>
 </div>

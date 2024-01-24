82e1d0b3fcc6ab872e17a2467d70ec8ec45f49ad
Andrey <andrey.develop.a@gmail.com>
2024-01-24 23:23:31 +0700

    feat: Класс маршрутизации Router, метод delete

--- app/views/posts/show.tpl.php
+++ app/views/posts/show.tpl.php
@@ -9,6 +9,11 @@
             <h1><?= h($post['title']) ?></h1>
             <div class="col-md-12">
                 <?= h($post['content']) ?>
+              <form action="/posts" method="post">
+                <input type="hidden" name="_method" value="DELETE">
+                <input type="hidden" name="id" value="<?= $post['id'] ?>">
+                <button class="btn btn-warning" type="submit">Delete</button>
+              </form>
             </div>
         </div>
     </div>

0ba1275d059dcf625ab8701786090b13179fe45e
Andrey <andrey.develop.a@gmail.com>
2024-02-17 02:59:46 +0700

    feat: Bridge pattern

--- app/views/posts/index.tpl.php
+++ app/views/posts/index.tpl.php
@@ -4,9 +4,54 @@
 /** @var Pagination $pagination */
 
 use Core\Pagination;
+use Core\Patterns\Bridge\Abstract\Page;
+use Core\Patterns\Bridge\Abstract\ProductPage;
+use Core\Patterns\Bridge\Abstract\SimplePage;
+use Core\Patterns\Bridge\HTMLRenderer;
+use Core\Patterns\Bridge\JsonRenderer;
+use Core\Patterns\Bridge\Product;
 
-require VIEWS . '/incs/header.php'
+require VIEWS . '/incs/header.php';
+
+
+function clientCode(Page $page)
+{
+    echo $page->view();
+}
+
+
+/**
+ * Клиентский код может выполняться с любой предварительно сконфигурированной
+ * комбинацией Абстракция+Реализация.
+ */
+$HTMLRenderer = new HTMLRenderer();
+$JSONRenderer = new JsonRenderer();
+
+$page = new SimplePage($HTMLRenderer, 'Home', 'Welcome to our website!');
+clientCode($page);
+echo "\n\n";
+/**
+ * При необходимости Абстракция может заменить связанную Реализацию во время
+ * выполнения.
+ */
+$page->changeRenderer($JSONRenderer);
+clientCode($page);
+echo "\n\n";
+
+
+$product = new Product('123', 'Star Wars, episode1',
+    'A long time ago in a galaxy far, far away...',
+    '/images/star-wars.jpeg', 39.95);
+
+$page = new ProductPage($HTMLRenderer, $product);
+clientCode($page);
+echo "\n\n";
+
+$page->changeRenderer($JSONRenderer);
+echo "JSON view of a simple content page, with the same client code:\n";
+clientCode($page);
 ?>
+
 
   <main class="main py-3">
     <div class="container">
